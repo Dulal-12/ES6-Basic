56229e575562d29f4b0082f36e9ca26b4e1ce3a1
Md.Dulal Miah <dulalmiah70112@gmail.com>
2021-02-02 05:44:57 +0600

    done string template

---
+++ template.js
@@ -0,0 +1,7 @@
+const firstname = 'Md. Dulal ';
+const lastname = 'Miah';
+const fullName = `${firstname} ${lastname}`;
+const text = `I write something for you . 
+You are special for me .  Will you marry me?`;
+//console.log(fullName);
+console.log(text);
