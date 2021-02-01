575e3f8eccfeb77cb59274752c3c5eff82d51391
Md.Dulal Miah <dulalmiah70112@gmail.com>
2021-02-02 05:24:15 +0600

    understand let , const , var declare problem

---
+++ letsconst.js
@@ -0,0 +1,19 @@
+//const => const 
+const wife = 'Fatema';
+
+//console.log(wife);
+
+const numbers  = [12,3,4];
+//You can change numbers array element. But You can't change Full array.
+//Example
+let name = 'Amin';
+name ='shamin';
+console.log(name); 
+numbers[1] = 34;
+console.log( numbers);
+
+//let is scope varaiable . Because this working in scope section . You don't acess this varaiable out of the scope.
+
+/* let declare => one time Value => change
+    const declare => one time value=> Not change
+    var declare => Many time value => change*/
