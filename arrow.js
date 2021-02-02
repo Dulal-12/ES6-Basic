127c24614f21d5cfbd04f0601f58718035a91f1f
Md.Dulal Miah <dulalmiah70112@gmail.com>
2021-02-02 06:01:37 +0600

    finally i am understand arrow function

---
+++ arrow.js
@@ -0,0 +1,21 @@
+/* We can write one function in three way . I will describe it */
+//1
+// function add (num1 , num2){
+//     return num1 + num2;
+// }
+
+//const result = add(23,4);
+//console.log(result);
+
+//2
+
+/*const add = function (num1 , num2){
+    return num1 + num2;
+}
+
+const result = add(23,4);
+console.log(result);*/
+
+//3
+const add = (num1 , num2)=> num1+ num2;
+console.log(add(34,5));
