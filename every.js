dfc3ed16e566d0758c3d6634c53ecc1b3d95c866
Md.Dulal Miah <dulalmiah70112@gmail.com>
2021-02-02 11:56:34 +0600

    done simple every method

---
+++ every.js
@@ -0,0 +1,8 @@
+//when all the value is true (value === condition)
+
+const arr = [1,1,1,1,1,1,2]
+const num = 1;
+const result = arr.every(value =>  value===num);
+console.log(result);
+
+//Insallah done every method 
